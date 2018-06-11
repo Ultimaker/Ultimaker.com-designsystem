180c9cf1fecac5457de185b87e1d8d8b2769d1a9
Robbert Helling <robbert@codebron.nl>
2018-06-11 15:16:24 +0200

    feat: added html only component

---
+++ src/welcome.stories.js
@@ -0,0 +1,9 @@
+import { storiesOf } from '@storybook/vue';
+
+storiesOf('global', module)
+    .add(
+        'Welcome',
+        () => ({
+            template: `<div><h2>Welcome</h2></div>`
+        })
+    );
