a14f909c3753d35145cb3563a5332034238a992d
Robbert Helling <robbert@codebron.nl>
2018-06-20 14:14:47 +0200

    fix: general-content story

--- src/stories/organisms/general-content/general-content.stories.js
+++ src/stories/organisms/general-content/general-content.stories.js
@@ -1,26 +1,32 @@
 import {storiesOf} from '@storybook/vue';
 import {withKnobs, text, object, boolean} from '@storybook/addon-knobs';
-import data from './general-content.data';
+import data from './general-content.stories.json';
 
 storiesOf('organisms|general-content', module)
     .addDecorator(withKnobs)
-    .add('General content', () => {
-        const title = text('Title', data.title),
-            content = text('Text', data.text),
-            link = text('Link', data.contentLink.text),
-            reversed = boolean('Reversed', data.reversed),
-            image = object('Image', data.image);
+    .add(
+        'General content',
+        () => {
+            const title = text('Title', data.title),
+                content = object('Text', data.text),
+                contentLink = object('Link', data.contentLink),
+                reversed = boolean('Reversed', data.reversed),
+                image = object('Image', data.image);
 
-        return {
-            data: () => ({
-                generalContent: {
-                    title,
-                    text: content,
-                    reversed,
-                    link,
-                    image
-                }
-            }),
-            template: `<general-content v-bind="generalContent"></general-content>`
-        };
-    });
+            return {
+                data: () => ({
+                    generalContent: {
+                        title,
+                        text: content,
+                        reversed,
+                        contentLink,
+                        image
+                    }
+                }),
+                template: require('./general-content.stories.html')
+            };
+        },
+        {
+            notes: {markdown: require('./general-content.stories.md')}
+        }
+    );
