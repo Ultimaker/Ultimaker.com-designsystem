ba42e8a007dcbb19698458a1cca564bd4f5469a5
Robbert Helling <robbert@codebron.nl>
2018-06-21 08:55:49 +0200

    fix: slider-reveal knobs

--- src/stories/organisms/slider-reveal/slider-reveal.stories.js
+++ src/stories/organisms/slider-reveal/slider-reveal.stories.js
@@ -1,17 +1,19 @@
 import {storiesOf} from '@storybook/vue';
-// import {withKnobs, text, object} from '@storybook/addon-knobs';
+import {withKnobs, select, text, object} from '@storybook/addon-knobs';
 import data from './slider-reveal.stories.json';
 
 storiesOf('organisms|slider-reveal', module)
-    // .addDecorator(withKnobs)
+    .addDecorator(withKnobs)
     .add('Slider reveal', () => {
         // Remark: slider-reveal doesn't initialize properly when
         // using knobs.
         //
-        // const title = text('Title', data.title),
-        //     contents = object('Contents', data.contents),
-        //     imageSlider = object('Images', data.imageSlider),
-        //     link = object('Link', data.link);
+        const modifier = select('Modifier', data.modifiers.map((b) => b.title), data.defaultModifier),
+            selectedModifier = data.modifiers.find((m) => m.title === modifier),
+            title = text('Title', data.title),
+            contents = object('Contents', data.contents),
+            imageSlider = object('Images', data.imageSlider),
+            link = object('Link', data.link);
 
         return {
             data: () => ({
@@ -20,7 +22,8 @@ storiesOf('organisms|slider-reveal', module)
                     contents: data.contents,
                     imageSlider: data.imageSlider,
                     link: data.link
-                }
+                },
+                modifier: selectedModifier.modifierClass
             }),
             template: require('./slider-reveal.stories.html')
         };
