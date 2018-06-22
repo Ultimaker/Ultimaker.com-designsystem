db565f4e372d1b291f15a4afa446a09e9f59b63e
Robbert Helling <robbert@codebron.nl>
2018-06-22 09:00:47 +0200

    feat: printers templates

--- src/stories/templates/printers/printers.stories.data.js
+++ src/stories/templates/printers/printers.stories.data.js
@@ -1,3 +1,9 @@
 module.exports = {
-    title: 'bla'
+    brands: require('../../organisms/brands/brands.stories.json'),
+    sliderReveal: require('../../organisms/slider-reveal/slider-reveal.stories.json'),
+    examples: require('../../organisms/examples/examples.stories.json'),
+    explore: require('../../organisms/explore/explore.stories.json'),
+    ecosystem: require('../../organisms/ecosystem/ecosystem.stories.json'),
+    cases: require('../../organisms/cases/cases.stories.json'),
+    generalContent: require('../../organisms/general-content/general-content.stories.json')
 };
