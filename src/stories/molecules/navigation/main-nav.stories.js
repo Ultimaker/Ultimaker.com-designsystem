fda326b1b4db1aca1d91d5eebcf5d21bd7b26dcf
Robbert Helling <robbert@codebron.nl>
2018-08-01 16:21:31 +0200

    feat: initial navigation story

--- src/stories/molecules/navigation/main-nav.stories.js
+++ src/stories/molecules/navigation/main-nav.stories.js
@@ -1,10 +1,14 @@
 import {storiesOf} from '@storybook/vue';
+import mainNavData from './main-nav.stories.data.js';
 
 storiesOf('Molecules|navigation', module)
     .add(
         'Main navigation',
         () => ({
-            template: require('./main-nav.stories.html')
+            template: require('./main-nav.stories.html'),
+            data: () => ({
+                mainNavData
+            })
         }),
         {
             notes: {markdown: require('./main-nav.stories.md')}
