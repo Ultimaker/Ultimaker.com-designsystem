b87fc827f878b0fca100a035fc4ece57bab39bb5
zwaardje <niels@seiton.io>
2018-09-10 11:34:02 +0200

    fix(resellers): fix card story mock data
    Image must be an object instead of an array.

--- src/stories/molecules/layout/card.stories.js
+++ src/stories/molecules/layout/card.stories.js
@@ -4,17 +4,14 @@ import knobs from './card.knobs.stories.js';
 
 storiesOf('Molecules|layout/cards', module)
     .addDecorator(withKnobs)
-    .add(
-        'Card',
-        () => {
-            return {
-                data: () => ({
-                    card: knobs()
-                }),
-                template: require('./card.stories.html')
-            };
-        },
-        {
-            notes: {markdown: require('./card.stories.md')}
-        }
-    );
+    .add('Card', () => {
+        return {
+            data: () => ({
+                card: knobs()
+            }),
+            template: require('./card.stories.html')
+        };
+    },
+    {
+        notes: {markdown: require('./card.stories.md')}
+    });
