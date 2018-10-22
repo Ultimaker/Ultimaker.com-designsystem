8cb757a8741f132352b9354eebb84d50db7cb163
Robbert Helling <robbert@codebron.nl>
2018-10-22 11:24:31 +0200

    chore: removed unused directive

--- src/js/directives/event/event.js
+++
@@ -1,6 +0,0 @@
-export default {
-    name: 'event',
-    bind(el, binding, vnode) {
-        console.info('event directive bound: ', el, binding, vnode);
-    }
-};
--- src/js/directives/index.js
+++
@@ -1,1 +0,0 @@
-export {default as Event} from './event/event';
