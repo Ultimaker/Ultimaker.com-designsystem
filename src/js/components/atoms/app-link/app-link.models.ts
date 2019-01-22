12ea9ca697a9026d247f69ce0c800c8342ac5385
Alexandros Chalvantzis <chalvantzis12@outlook.com>
2019-01-22 15:09:35 +0100

    modeling the app link component

---
+++ src/js/components/atoms/app-link/app-link.models.ts
@@ -0,0 +1,5 @@
+import { Button } from "@ultimaker/ultimaker.com-model-definitions/dist/atoms/cta/Button";
+
+export interface IAppLink extends Button {
+    to: string;
+}
--- src/js/components/atoms/app-link/app-link.ts
+++ src/js/components/atoms/app-link/app-link.ts
@@ -1,12 +1,15 @@
 import { Vue, Component, Prop } from 'vue-property-decorator';
+import { IAppLink } from 'src/js/components/atoms/app-link/app-link.models';
 
 @Component({
     name: 'app-link',
     template: require('./app-link.html'),
 })
 
-export default class AppLink extends Vue {
+export default class AppLink extends Vue implements IAppLink {
     @Prop({ type: String, required: true }) to!: string;
+    @Prop({ type: String, default: '' }) url!: string;
+    @Prop({ type: String, default: '' }) label!: string;
 
     public linkProps():Object {
         if (this.to.match(/^(http(s)?):\/\//)) {
