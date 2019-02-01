040a514329e7b02f3cfb0a4f6b7cc43eca1ce8b2
Alexandros Chalvantzis <chalvantzis12@outlook.com>
2019-02-01 13:43:29 +0100

    update unit test for cta-block molecule

--- src/js/components/molecules/cta-block/cta-block.unit.spec.js
+++ src/js/components/molecules/cta-block/cta-block.unit.spec.js
@@ -5,14 +5,52 @@ import {build} from 'vuenit';
 describe('components', () => {
     describe('molecules', () => {
         describe('cta-block', () => {
-            const mount = build(CtaBlock);
+            const mount = build(CtaBlock, {});
 
-            it('should render a "cta-block"', () => {
-                const ctas = {button: {uri: '#', title: 'Narwals rule!'}};
-                const vm = mount({ctas});
+            describe('should not render', () => {
+                const buildOptions = {
+                    props: {
+                        ctas: []
+                    }
+                };
+                const vm = mount(buildOptions);
 
-                expect(vm.$el).toBeDefined();
-                vm.$destroy();
+                it('should not render a "cta-block"', () => {
+                    const ctaBlockButtons = vm.$el.childNodes.length;
+
+                    expect(ctaBlockButtons).toBe(0);
+                    vm.$destroy();
+                });
+            });
+
+            describe('should render', () => {
+                const buildOptions = {
+                    props: {
+                        ctas: [
+                            {
+                                'type': 'ContentButton',
+                                'label': 'I am label',
+                                'url': 'https://google.com/'
+                            },
+                            {
+                                'type': 'ContentLink',
+                                'label': 'I am label',
+                                'url': 'https://google.com/'
+                            }
+                        ]
+                    }
+                };
+
+                const vm = mount(buildOptions);
+
+                it('should render a "cta-block" with ctas buttons', () => {
+                    const ctaBlockButtons = vm.$el.childNodes.length;
+
+                    expect(vm.$el).toBeDefined();
+                    expect(ctaBlockButtons).toBeGreaterThanOrEqual(1);
+
+                    vm.$destroy();
+                });
             });
         });
     });
