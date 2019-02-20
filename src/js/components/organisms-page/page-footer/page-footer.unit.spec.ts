db019d1b9b5369fa34a8566e49e001e2ec7ab5be
Zwaardje <niels@nielsaulman.nl>
2019-02-20 10:57:09 +0100

    chore(bump): bump version
    bump version
    sync packagelock

--- src/js/components/organisms-page/page-footer/page-footer.unit.spec.ts
+++ src/js/components/organisms-page/page-footer/page-footer.unit.spec.ts
@@ -1,17 +1,17 @@
 /* eslint-disable max-nested-callbacks */
 import PageFooter from './page-footer';
-import {build} from 'vuenit';
+import { build } from 'vuenit';
 
 describe('components', () => {
     describe('layout', () => {
         describe('page-footer', () => {
-            const data = require('./page-footer.unit.spec.json'),
-                buildOptions = {props: {...data}},
-                mount = build(PageFooter, {buildOptions});
+            const data = require('./page-footer.unit.spec.json');
+            const buildOptions = { props: { ...data } };
+            const mount = build(PageFooter, { buildOptions });
 
             it('should render a page-footer element', () => {
-                const vm = mount(buildOptions),
-                    objAttributes = vm.$el.attributes;
+                const vm = mount(buildOptions);
+                const objAttributes = vm.$el.attributes;
 
                 expect(vm.$el).toBeDefined();
                 expect(objAttributes['class'].value).toContain('footer');
