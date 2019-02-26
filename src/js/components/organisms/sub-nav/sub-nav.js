b4c48baa1091f4762dedd2ce6875183f513f9414
Robbert Helling <robbert@codebron.nl>
2019-02-26 08:28:21 +0100

    fix(sysnav): remove vuex reference, will be fixed later

--- src/js/components/organisms/sub-nav/sub-nav.js
+++ src/js/components/organisms/sub-nav/sub-nav.js
@@ -1,25 +1,18 @@
 import ViewportUtil from 'utils/viewport';
-import {mapGetters} from 'vuex';
 
 export default {
     name: 'sub-nav',
     template: require('./sub-nav.html'),
     data: () => ({
-        viewport: new ViewportUtil()
+        viewport: new ViewportUtil(),
+        page: null
     }),
     computed: {
         classObject() {
             return {
                 'sub-nav--collapsed': this.viewport.scrollY > 75 && this.viewport.scrollOffset > -75
             };
-        },
-        ...mapGetters('page', [
-            'page',
-            'pageLoading'
-        ])
-    },
-    methods: {
-        mapLink: (link) => link.replace(/^\/api\/pages/, '')
+        }
     }
 };
 
