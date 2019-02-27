75d70fd50e80e016258bd735b6e6f414e7bca64b
Robbert Helling <robbert@codebron.nl>
2019-02-27 08:00:11 +0100

    fix(header): allow icons to be configured in the cms

--- src/js/components/molecules/sys-nav/sys-nav.ts
+++ src/js/components/molecules/sys-nav/sys-nav.ts
@@ -5,8 +5,14 @@ import { Component, Prop, Vue } from 'vue-property-decorator';
     template: require('./sys-nav.html'),
 })
 export default class SysNav extends Vue {
+    @Prop({ type: String, default: 'store' })
+    orderNowIcon!: string;
+
     @Prop({ type: String, required: true })
     orderNowLabel!: string;
+
+    @Prop({ type: String, default: 'search' })
+    searchIcon!: string;
 
     @Prop({ type: String, required: true })
     searchLabel!: string;
