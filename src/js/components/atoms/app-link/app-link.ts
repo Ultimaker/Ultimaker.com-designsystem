5ff22bbc11c10c5690a87842a1a22a07793478e4
Robbert Helling <robbert@codebron.nl>
2019-02-27 07:51:17 +0100

    fix(header): no router links for now

--- src/js/components/atoms/app-link/app-link.ts
+++ src/js/components/atoms/app-link/app-link.ts
@@ -12,17 +12,11 @@ export default class AppLink extends Vue implements IAppLink {
     @Prop({ type: String, default: '' }) label!: string;
 
     public linkProps():Object {
-        if (this.to.match(/^(http(s)?):\/\//)) {
-            return {
-                is: 'a',
-                href: this.to,
-                target: '_blank',
-                rel: 'noopener',
-            };
-        }
         return {
-            is: 'router-link',
-            to: this.to,
+            is: 'a',
+            href: this.to,
+            target: '_blank',
+            rel: 'noopener',
         };
     }
 }
