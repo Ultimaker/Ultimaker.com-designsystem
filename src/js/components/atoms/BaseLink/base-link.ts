b1cff8f8f30819c83188291e2ed26031cfeee531
zwaardje <niels@seiton.io>
2019-02-28 09:16:31 +0100

    fix(content-link): remove baslink hack
    Remove base link hack. Fix content links.
    Fix sys nav order now link
    Bump version

--- src/js/components/atoms/BaseLink/base-link.ts
+++ src/js/components/atoms/BaseLink/base-link.ts
@@ -12,14 +12,9 @@ export default class BaseLink extends Vue implements IBaseLink {
     @Prop({ type: String }) icon?: IBaseLink['icon'];
     @Prop({ type: String, default: '' }) url!: IBaseLink['url'];
     @Prop({ type: String, default: '' }) label!: IBaseLink['label'];
-    @Prop({ type: String, default: '' }) href!: IBaseLink['url'];
 
     absoluteUrlRegex: RegExp = /^(http(s)?):\/\//;
     domainRegex: RegExp = /(http(s)?):\/\/(www.)?ultimaker\.com/;
-
-    get link () {
-        return this.url || this.href;
-    }
 
     get classObject() {
         const classes = {};
@@ -40,17 +35,17 @@ export default class BaseLink extends Vue implements IBaseLink {
     }
 
     get linkProps(): object {
-        if (this.link.match(this.absoluteUrlRegex)) {
+        if (this.url.match(this.absoluteUrlRegex)) {
             return {
                 is: 'a',
-                href: this.link,
-                target: this.link.match(this.domainRegex) ? '_self' : '_blank',
+                href: this.url,
+                target: this.url.match(this.domainRegex) ? '_self' : '_blank',
                 rel: 'noopener',
             };
         }
         return {
             is: 'router-link',
-            to: this.link,
+            to: this.url,
         };
     }
 }
