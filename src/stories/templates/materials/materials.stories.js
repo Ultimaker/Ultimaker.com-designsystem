09e521dfe1b42a393468120ad7ec38618996f59d
dan entous <d.entous@ultimaker.com>
2019-04-13 12:13:48 +0200

    fix(materials story)

--- src/stories/templates/materials/materials.stories.js
+++ src/stories/templates/materials/materials.stories.js
@@ -2,22 +2,23 @@ import {storiesOf} from '@storybook/vue';
 import {withKnobs} from '@storybook/addon-knobs';
 import getKnobs from './materials.knobs.stories.js';
 
-const knobs = getKnobs();
-
 storiesOf('templates|materials', module)
     .addDecorator(withKnobs)
-    .add('Materials', () => ({
-        data: () => ({
+    .add('Materials',
+        () => {
+            const knobs = getKnobs();
 
-        }),
-        props: {
-            headerKnobs: {default: knobs.headerKnobs},
-            subNavigationKnobs: {default: knobs.subNavigationKnobs},
-            footerKnobs: {default: knobs.footerKnobs},
-            heroVideo: {default: knobs.heroVideo}
+            return {
+                props: {
+                    headerKnobs: {default: knobs.headerKnobs},
+                    subNavigationKnobs: {default: knobs.subNavigationKnobs},
+                    footerKnobs: {default: knobs.footerKnobs},
+                    heroVideo: {default: knobs.heroVideo}
+                },
+                template: require('./materials.stories.html')
+            };
         },
-        template: require('./materials.stories.html')
-    }),
-    {
-        notes: {markdown: require('./materials.stories.md')}
-    });
+        {
+            notes: {markdown: require('./materials.stories.md')}
+        }
+    );
