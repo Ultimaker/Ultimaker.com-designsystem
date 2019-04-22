3c864e5755679132bd5d23cdb86685c660388550
dan entous <d.entous@ultimaker.com>
2019-04-22 09:20:55 +0200

    refactor(stories): create molecules/content-toggle story

---
+++ src/js/components/molecules/content-toggle/data/examples/content-toggle.data.ts
@@ -0,0 +1,11 @@
+const data = {
+    default: {
+        collapsedIcon: 'angle-down',
+        collapsedText: 'More',
+        expandedIcon: 'angle-up',
+        expandedText: 'Less',
+        toggleState: false,
+    },
+};
+
+export default type => data[type];
---
+++ src/js/components/molecules/content-toggle/stories/content-toggle.stories.ts
@@ -0,0 +1,48 @@
+import { storiesOf } from '@storybook/vue';
+import { action } from '@storybook/addon-actions';
+import { withKnobs } from '@storybook/addon-knobs';
+import getKnobs from '../data/knobs/content-toggle.knobs';
+
+const getNotes = () => {
+    return {
+        notes: require('./content-toggle.stories.md'),
+    };
+};
+
+const getStory = (type) => {
+    return () => {
+        const knobs = getKnobs(type);
+
+        return {
+            methods: {
+                toggle: action('toggle'),
+            },
+            props: {
+                collapsedIcon: {
+                    default: knobs.collapsedIcon,
+                },
+                collapsedText: {
+                    default: knobs.collapsedText,
+                },
+                expandedIcon: {
+                    default: knobs.expandedIcon,
+                },
+                expandedText: {
+                    default: knobs.expandedText,
+                },
+                toggleState: {
+                    default: knobs.toggleState,
+                },
+            },
+            template: require('./content-toggle.stories.html'),
+        };
+    };
+};
+
+storiesOf('Molecules|buttons/content-toggle', module)
+    .addDecorator(withKnobs)
+    .add(
+        'default',
+        getStory('default'),
+        getNotes(),
+    );
