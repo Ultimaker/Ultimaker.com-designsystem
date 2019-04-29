95e7d01d042dde0891eab66c1d1448ee0d3c87df
Etiënne van den Tempel <e.vandentempel@ultimaker.com>
2019-04-29 12:26:25 +0200

    Remove conditional grid-classes for the OverviewUbr

--- src/js/components/organisms-page/overview-ubr/overview-ubr.ts
+++ src/js/components/organisms-page/overview-ubr/overview-ubr.ts
@@ -12,22 +12,4 @@ export default class OverviewUBR extends Vue implements OverviewUBRInterface {
     @Prop({ type: String, required: false }) subtitle?: OverviewUBRInterface['subtitle'];
     @Prop({ type: Array, required: true }) items!: OverviewUBRInterface['items'];
     @Prop({ type: Object, required: false }) ctas?: OverviewUBRInterface['ctas'];
-
-    get flexgridClasses() {
-        const amountOfItems: number = this.items.length;
-
-        if (amountOfItems === 2) {
-            return 'flexgrid__cell--xs-4 flexgrid__cell--lg-3';
-        }
-
-        if (amountOfItems === 3) {
-            return 'flexgrid__cell--xs-6 flexgrid__cell--md-4';
-        }
-
-        if (amountOfItems === 4) {
-            return 'flexgrid__cell--md-6 flexgrid__cell--lg-3';
-        }
-
-        return 'flexgrid__cell--xs-6 flexgrid__cell--md-4';
-    }
 }
