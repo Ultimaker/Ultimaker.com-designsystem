6a81e0e60b25be2f513ac3e366ff31fb6f7bb518
dan entous <d.entous@ultimaker.com>
2019-04-29 14:33:09 +0200

    refactor(stories): remove old stories

--- src/stories/atoms/text/text.stories.js
+++ src/stories/atoms/text/text.stories.js
@@ -1,17 +1,6 @@
 import {storiesOf} from '@storybook/vue';
 
 storiesOf('Atoms|text', module)
-
-    .add(
-        'Copy',
-        () => ({
-            inject: ['data'],
-            template: require('./text.copy.stories.html')
-        }),
-        {
-            notes: {markdown: require('./text.copy.stories.md')}
-        }
-    )
     .add(
         'Inline elements',
         () => ({
