2bbec9655add2f84ddad4034bc34d06ee53ce061
dan entous <d.entous@ultimaker.com>
2019-04-30 13:11:46 +0200

    refactor(storeis): add code

---
+++ src/js/components/atoms/text/inline/code/data/code.data.ts
@@ -0,0 +1,7 @@
+export const data = {
+    default: {
+        text: 'alert(”i got your cookie!” + document.cookie);',
+    },
+};
+
+export default type => data[type];
---
+++ src/js/components/atoms/text/inline/code/stories/code.stories.ts
@@ -0,0 +1,32 @@
+import addNotes from 'src/stories/helpers/add-notes';
+import addStories from 'src/stories/helpers/add-stories';
+import getKnobs from 'src/stories/helpers/get-knobs';
+
+import { data } from '../data/code.data';
+import knobsFormat from './code.knobs-format';
+
+const getStory = (type) => {
+    return () => {
+        const knobs = getKnobs(
+            data[type],
+            knobsFormat,
+        );
+
+        return {
+            props: {
+                text: {
+                    default: knobs.text,
+                },
+            },
+            template: '<code>{{ text }}</code>',
+        };
+    };
+};
+
+addStories({
+    data,
+    getStory,
+    decorators: ['withKnobs'],
+    kind: 'Atoms|text/inline/code',
+    notes: addNotes(require('../../inline.md')),
+});
