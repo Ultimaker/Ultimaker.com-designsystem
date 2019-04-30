19e4500c93f5fd5ad44e878793faed515c5c1ba4
dan entous <d.entous@ultimaker.com>
2019-04-30 13:30:52 +0200

    refactor(stories): add a

---
+++ src/js/components/atoms/text/inline/a/data/a.data.ts
@@ -0,0 +1,8 @@
+export const data = {
+    default: {
+        href: 'https://ultimaker.com/',
+        text: 'Ultimaker',
+    },
+};
+
+export default type => data[type];
---
+++ src/js/components/atoms/text/inline/a/stories/a.stories.ts
@@ -0,0 +1,35 @@
+import addNotes from 'src/stories/helpers/add-notes';
+import addStories from 'src/stories/helpers/add-stories';
+import getKnobs from 'src/stories/helpers/get-knobs';
+
+import { data } from '../data/a.data';
+import knobsFormat from './a.knobs-format';
+
+const getStory = (type) => {
+    return () => {
+        const knobs = getKnobs(
+            data[type],
+            knobsFormat,
+        );
+
+        return {
+            props: {
+                href: {
+                    default: knobs.href,
+                },
+                text: {
+                    default: knobs.text,
+                },
+            },
+            template: '<a :href="href">{{ text }}</a:href>',
+        };
+    };
+};
+
+addStories({
+    data,
+    getStory,
+    decorators: ['withKnobs'],
+    kind: 'Atoms|text/inline/a',
+    notes: addNotes(require('../../inline.md')),
+});
