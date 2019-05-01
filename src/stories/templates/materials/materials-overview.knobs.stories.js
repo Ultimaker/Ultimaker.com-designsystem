ac6ab40fe8abd14207f1222e33de4e38eae77fbc
Robbert Helling <robbert@codebron.nl>
2019-05-01 15:09:57 +0200

    chore: unused files

--- src/stories/templates/materials/materials-overview.knobs.stories.js
+++
@@ -1,7 +0,0 @@
-import heroCoverKnobs from '../../organisms/hero-cover/hero-cover.knobs.stories.js';
-import footerKnobs from '../../organisms/page-footer/page-footer.knobs.stories';
-
-export default () => ({
-    ... heroCoverKnobs(),
-    ... footerKnobs()
-});
