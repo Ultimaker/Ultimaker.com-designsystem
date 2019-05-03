62da18e1d17d24e11fa50a0c35f9987cb47aa9a2
Robbert Helling <robbert@codebron.nl>
2019-05-03 16:16:43 +0200

    feature(resellers): add resellers storybook template

---
+++ src/stories/templates/resellers/resellers.stories.js
@@ -0,0 +1,27 @@
+import {storiesOf} from '@storybook/vue';
+import {withKnobs} from '@storybook/addon-knobs';
+import headerKnobs from 'src/stories/organisms/page-header/page-header.knobs.stories';
+import footerKnobs from 'src/stories/organisms/page-footer/page-footer.knobs.stories';
+import heroBasicKnobs from '../../organisms/hero-basic/hero-basic.knobs.stories';
+import businessCardData from 'organisms-page/overview/data/examples/business-cards';
+
+storiesOf('templates|resellers', module)
+    .addDecorator(withKnobs)
+    .add('Resellers overview',
+        () => {
+            return {
+                data: () => ({
+                    ... headerKnobs(),
+                    ... footerKnobs()
+                }),
+                props: {
+                    heroBasic: {default: heroBasicKnobs().heroBasic},
+                    overviewData: {default: businessCardData}
+                },
+                template: require('./resellers.stories.html')
+            };
+        },
+        {
+            notes: {markdown: require('./resellers.stories.md')}
+        }
+    );
---
+++ src/stories/templates/resellers/resellers.stories.md
@@ -0,0 +1,3 @@
+# Resellers
+
+The basic template for resellers overview page.
