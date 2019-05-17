beaa045055a37de015583b457b236a1235d298aa
Robbert Helling <robbert@codebron.nl>
2019-05-17 11:43:22 +0200

    feat(table-compare): mockdata, very early implementation

--- src/js/components/organisms/table-compare/data/table-compare.data.ts
+++ src/js/components/organisms/table-compare/data/table-compare.data.ts
@@ -1,8 +1,102 @@
+import { data as imageData } from 'components/atoms/c-image/data/c-image.data';
+
+const columnCta = {
+    type: 'ContentButton',
+    label: 'Get a quote',
+    url: '/en/quote',
+};
+
 export const data = {
     default: {
         title: 'Lorem ipsum dolor sit amet consectetur',
         subtitle: 'Donec pulvinar arcu lorem, id scelerisque lectus cursus in',
-        content: {},
+        content: {
+            columns: [
+                {
+                    title: 'Printer One',
+                    image: imageData.default,
+                    cta: columnCta,
+                },
+                {
+                    title: 'Printer Two',
+                    image: imageData.default,
+                    cta: columnCta,
+                },
+                {
+                    title: 'Printer Three',
+                    image: imageData.default,
+                    cta: columnCta,
+                },
+            ],
+            rows: [
+                {
+                    label: 'Feature One',
+                    tooltip: {
+                        label: '',
+                        icon: 'info',
+                        description: 'Celery quandong swiss chard chicory earthnut pea potato. Salsify taro catsear garlic gram celery bitterleaf wattle seed collard greens nori.',
+                    },
+                    cells: [
+                        {
+                            checked: false,
+                            label: '',
+                        },
+                        {
+                            checked: true,
+                            label: '',
+                        },
+                        {
+                            checked: true,
+                            label: '',
+                        },
+                    ],
+                },
+                {
+                    label: 'Feature Two',
+                    tooltip: {
+                        label: '',
+                        icon: 'info',
+                        description: 'Celery quandong swiss chard chicory earthnut pea potato. Salsify taro catsear garlic gram celery bitterleaf wattle seed collard greens nori.',
+                    },
+                    cells: [
+                        {
+                            checked: true,
+                            label: '',
+                        },
+                        {
+                            checked: true,
+                            label: '',
+                        },
+                        {
+                            checked: true,
+                            label: '',
+                        },
+                    ],
+                },
+                {
+                    label: 'Feature Three',
+                    tooltip: {
+                        label: '',
+                        icon: 'info',
+                        description: 'Celery quandong swiss chard chicory earthnut pea potato. Salsify taro catsear garlic gram celery bitterleaf wattle seed collard greens nori.',
+                    },
+                    cells: [
+                        {
+                            checked: true,
+                            label: '',
+                        },
+                        {
+                            checked: true,
+                            label: '',
+                        },
+                        {
+                            checked: false,
+                            label: '',
+                        },
+                    ],
+                },
+            ],
+        },
         ctas: {
             type: 'Ctas',
             ctas: [
