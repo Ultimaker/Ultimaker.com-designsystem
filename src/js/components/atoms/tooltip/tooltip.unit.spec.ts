89041ed71efab942b43c9515743d7b42fe642cb7
Matijs Brinkhuis <matijs@gmail.com>
2019-05-20 08:13:05 +0200

    refactor(tooltip): Don't reuse the view model between tests

--- src/js/components/atoms/tooltip/tooltip.unit.spec.ts
+++ src/js/components/atoms/tooltip/tooltip.unit.spec.ts
@@ -4,20 +4,10 @@ import { build } from 'vuenit';
 
 describe('components', () => {
     describe('atoms', () => {
-        let mount;
-        let vm;
-
-        beforeEach(() => {
-            mount = build(Tooltip);
-        });
-
-        afterEach(() => {
-            vm.$destroy();
-        })
-
         describe('tooltip', () => {
+            const mount = build(Tooltip);
             it('should render a tooltip with a block property', () => {
-                vm = mount({
+                const vm = mount({
                         props: {
                             block: 'test',
                         },
@@ -25,14 +15,18 @@ describe('components', () => {
 
                 const objAttributes = vm.$el.attributes;
                 expect(objAttributes['class'].value).toBe('tooltip test__tooltip');
+                vm.$destroy();
             });
         });
+
         describe('tooltip without a block', () => {
+            const mount = build(Tooltip);
             it('should render a tooltip with a block property', () => {
                 const vm = mount().$mount();
 
                 const objAttributes = vm.$el.attributes;
                 expect(objAttributes['class'].value).toBe('tooltip');
+                vm.$destroy();
             });
         });
     });
