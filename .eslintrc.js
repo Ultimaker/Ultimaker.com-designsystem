c015f1df4671c8d78cd56f3510b67cd876e07426
Matijs Brinkhuis <matijs@gmail.com>
2019-06-04 16:42:23 +0200

    chore(prettier): add trailingComma to prettier.config.js
    Also format the .eslintrc.js

--- .eslintrc.js
+++ .eslintrc.js
@@ -1,3 +1,5 @@
+/** @format */
+
 // http://eslint.org/docs/user-guide/configuring
 
 module.exports = {
@@ -5,31 +7,29 @@ module.exports = {
     parser: '@typescript-eslint/parser',
     parserOptions: {
         ecmaVersion: 2019,
-        sourceType: 'module'
+        sourceType: 'module',
     },
     env: {
         amd: true,
         browser: true,
         es6: true,
         jasmine: true,
-        node: true
+        node: true,
     },
-    plugins: [
-        '@typescript-eslint',
-    ],
+    plugins: ['@typescript-eslint'],
     extends: [
         // 'plugin:@typescript-eslint/recommended',
-        'eslint-config-airbnb-base'
+        'eslint-config-airbnb-base',
     ],
     globals: {
-        'process': true,
-        'module': true,
-        'exports': true
+        process: true,
+        module: true,
+        exports: true,
     },
     rules: {
         'class-methods-use-this': 'off',
         'global-require': 'off',
-        'indent': ['error', 4, { 'SwitchCase': 1 }],
+        indent: ['error', 4, { SwitchCase: 1 }],
         'import/newline-after-import': 'off',
         'import/no-extraneous-dependencies': 'off',
         'import/no-unresolved': 'off',
@@ -39,12 +39,11 @@ module.exports = {
         'max-len': 'off',
         'max-line-length': 'off',
         'new-parens': 'off',
-        'no-console': ["error", { allow: ["info", "warn", "error"] }],
+        'no-console': ['error', { allow: ['info', 'warn', 'error'] }],
         'no-underscore-dangle': 'off',
         'no-unused-vars': 'off',
         'object-curly-newline': 'off',
         'operator-linebreak': 'off',
         'prefer-default-export': 'off',
-    }
+    },
 };
-
--- prettier.config.js
+++ prettier.config.js
@@ -2,12 +2,13 @@
 module.exports = {
     requirePragma: true,
     singleQuote: true,
+    trailingComma: 'es5',
     overrides: [
         {
             files: '*.scss',
             options: {
-                singleQuote: false
-            }
-        }
-    ]
+                singleQuote: false,
+            },
+        },
+    ],
 };
