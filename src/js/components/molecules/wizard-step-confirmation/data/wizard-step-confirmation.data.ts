786d0b930625cb82666f4e2b2a02f4a3cecf915b
zwaardje <niels@seiton.io>
2019-06-06 15:06:29 +0200

    feat(thank-you): add thank you
    Add conformation
    Build etc etc

--- src/js/components/molecules/wizard-step-confirmation/data/wizard-step-confirmation.data.ts
+++ src/js/components/molecules/wizard-step-confirmation/data/wizard-step-confirmation.data.ts
@@ -1,4 +1,29 @@
 export const data = {
     default: {
-    }
-}
+        title: 'Title',
+        subtitle: 'Subtitle',
+        contentTitle: 'Content title',
+        content: 'Content string',
+        image: {
+            url: 'https://images.ctfassets.net/yadj1kx9rmg0/wtrHxeu3zEoEce2MokCSi/cf6f68efdcf625fdc060607df0f3baef/quwowooybuqbl6ntboz3.jpg',
+            alt: 'An example image',
+            quality: 65,
+            radius: 0,
+            imageFormat: 'png',
+            resizeBehavior: 'fill',
+            focusArea: 'center',
+            backgroundColor: null,
+            mimeType: 'png',
+        },
+        ctas: {
+            type: 'CtaBlock',
+            ctas: [
+                {
+                    type: 'ContentButton',
+                    label: 'Call me back',
+                    url: 'https://ultimaker.com',
+                },
+            ],
+        },
+    },
+};
--- src/js/components/molecules/wizard-step-confirmation/stories/wizard-step-confirmation.knobs-format.ts
+++ src/js/components/molecules/wizard-step-confirmation/stories/wizard-step-confirmation.knobs-format.ts
@@ -1,2 +1,8 @@
 export default {
+    title: '',
+    subtitle: '',
+    contentTitle: '',
+    content: '',
+    image: {},
+    ctas: {},
 };
