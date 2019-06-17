9697dfec0f3e40dc240d23cd534a1c5111f08330
zwaardje <niels@seiton.io>
2019-06-17 14:26:30 +0200

    chore(fixes): add hero fixes

--- src/js/components/organisms/hero-article/data/hero-article.data.ts
+++ src/js/components/organisms/hero-article/data/hero-article.data.ts
@@ -1,6 +1,15 @@
 export const data = {
     default: {
         title: 'Im the title of the article',
+        image: {
+            alt: 'bla bla',
+            url: '/generator/svg/1500/400/000022/FFFFFF',
+            mimeType: 'image/svg+xml',
+            focusArea: 'center',
+        },
+    },
+    subtitle: {
+        title: 'Im the title of the article',
         subtitle: 'Im a subtle subtitle',
         image: {
             alt: 'bla bla',
