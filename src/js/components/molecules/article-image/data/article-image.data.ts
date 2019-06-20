86b0a5c229819783b3d2ad8629ffbed3a5e242c4
zwaardje <niels@seiton.io>
2019-06-20 14:59:09 +0200

    chore(css): fix css

--- src/js/components/molecules/article-image/data/article-image.data.ts
+++ src/js/components/molecules/article-image/data/article-image.data.ts
@@ -3,10 +3,19 @@ export const data = {
         type: 'ArticleImage',
         image: {
             alt: 'bla bla',
-            url: '/generator/svg/1500/400/000022/FFFFFF',
+            url: 'https://images.ctfassets.net/7cnpidfipnrw/5pg6zrePnjjv4NZMPUy5tu/99c55fd418fb1ea55920d3a5cf54d85e/4S5A1853.jpg',
             mimeType: 'image/svg+xml',
             focusArea: 'center',
         },
         caption: 'Im the caption of the image',
+    },
+    noCaption: {
+        type: 'ArticleImage',
+        image: {
+            alt: 'bla bla',
+            url: '/generator/svg/1500/400/000022/FFFFFF',
+            mimeType: 'image/svg+xml',
+            focusArea: 'center',
+        },
     },
 };
