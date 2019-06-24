0a35d8cbbb20b984a6b82b6f9a41f37cc7ba743b
zwaardje <niels@seiton.io>
2019-06-24 16:56:12 +0200

    chore(build): add build

--- src/js/utils/format.ts
+++ src/js/utils/format.ts
@@ -1,10 +1,12 @@
 import Default from '../constants/defaults';
 
-// Eventualy the locale will be passed down 
-// eg: publishedDate {
-//      date: '24-05-2019',
-//      locale: 'nl_NL',
-// }
+/* Eventualy the locale will be passed down
+ eg: publishedDate {
+     date: '24-05-2019',
+     locale: 'nl_NL',
+}
+*/
+
 export class Format {
     static options: object = {
         year: 'numeric',
