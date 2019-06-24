44db5cbb69d73e826671bbe88e6b472ffe3b4377
zwaardje <niels@seiton.io>
2019-06-24 11:58:32 +0200

    fix(article): css fixes intl format date

--- src/js/components/molecules/article-author/article-author.ts
+++ src/js/components/molecules/article-author/article-author.ts
@@ -1,6 +1,8 @@
 import { Vue, Component, Prop } from 'vue-property-decorator';
 import { ArticleAuthorProps } from './article-author.models';
 import WithRender from './article-author.vue.html';
+import { Format } from '../../../utils/format';
+import { format } from 'url';
 
 @WithRender
 @Component({
@@ -11,4 +13,10 @@ export class ArticleAuthor extends Vue implements ArticleAuthorProps {
     @Prop({ type: String, required: true }) name!: ArticleAuthorProps['name'];
     @Prop({ type: Object, required: false }) image?: ArticleAuthorProps['image'];
     @Prop({ type: String, required: true }) publishedDate!: ArticleAuthorProps['publishedDate'];
+
+    format = new Format();
+
+    get formatedDate() {
+        return this.format.ISODate(this.publishedDate);
+    }
 }
