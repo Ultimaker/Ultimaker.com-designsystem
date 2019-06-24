65a9524361ef54e7f6506eedeca2d7ba9f876a72
zwaardje <niels@seiton.io>
2019-06-24 13:52:27 +0200

    fix(comments): update comments

--- src/js/components/molecules/article-author/article-author.ts
+++ src/js/components/molecules/article-author/article-author.ts
@@ -2,7 +2,6 @@ import { Vue, Component, Prop } from 'vue-property-decorator';
 import { ArticleAuthorProps } from './article-author.models';
 import WithRender from './article-author.vue.html';
 import { Format } from '../../../utils/format';
-import { format } from 'url';
 
 @WithRender
 @Component({
@@ -14,9 +13,7 @@ export class ArticleAuthor extends Vue implements ArticleAuthorProps {
     @Prop({ type: Object, required: false }) image?: ArticleAuthorProps['image'];
     @Prop({ type: String, required: true }) publishedDate!: ArticleAuthorProps['publishedDate'];
 
-    format = new Format();
-
     get formatedDate() {
-        return this.format.ISODate(this.publishedDate);
+        return Format.ISODate(this.publishedDate);
     }
 }
--- src/js/utils/format.ts
+++ src/js/utils/format.ts
@@ -1,15 +1,15 @@
 import Default from '../constants/defaults';
 
 export class Format {
-    options = {
+    static options: object = {
         year: 'numeric',
         month: 'long',
         day: '2-digit',
     };
 
-    locale = `${Default.language}-${Default.country.code}`;
+    static locale: string = `${Default.language}-${Default.country.code}`;
 
-    ISODate(date: string, locale:string = this.locale, options: object = this.options): string {
+    static ISODate(date: string, locale:string = this.locale, options: object = this.options): string {
         const dateObj = new Date(date);
         return new Intl.DateTimeFormat(locale, options).format(dateObj);
     }
