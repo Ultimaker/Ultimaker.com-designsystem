b4a762a29b8d8357a9f45b91b605b065d209802f
Matijs Brinkhuis <matijs@gmail.com>
2019-07-09 15:17:07 +0200

    feat(social-sharing): Add social sharing
    Add the social sharing component at the bottom of an article

--- src/js/components/molecules/social-sharing/data/social-sharing.data.ts
+++ src/js/components/molecules/social-sharing/data/social-sharing.data.ts
@@ -4,18 +4,17 @@ export const data = {
     default: {
         items: [
             {
+                type: 'linkedin',
                 iconName: 'linkedin',
                 label: 'Share on Linkedin',
             },
             {
-                iconName: 'instagram',
-                label: 'Share on Instagram',
-            },
-            {
+                type: 'facebook',
                 iconName: 'facebook',
                 label: 'Share on Facebook',
             },
             {
+                type: 'twitter',
                 iconName: 'twitter',
                 label: 'Share on Twitter',
             },
