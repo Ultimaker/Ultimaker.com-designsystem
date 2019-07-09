52661c30efded21215c89c951bd8558513fcb6ce
Matijs Brinkhuis <matijs@gmail.com>
2019-07-09 15:17:07 +0200

    feat(social-sharing): Add social sharing
    Add the social sharing component at the bottom of an article

--- src/js/components/molecules/social-sharing/social-sharing.ts
+++ src/js/components/molecules/social-sharing/social-sharing.ts
@@ -1,13 +1,41 @@
 /** @format */
 
 import { Vue, Component, Prop } from 'vue-property-decorator';
-import { SocialSharingProps } from './social-sharing.models';
 import WithRender from './social-sharing.vue.html';
+import { generateSocialSharingUrl } from '../../../utils/generate-social-sharing-url';
+import BrowserCapabilities from '../../../utils/browser-capabilities';
 
 @WithRender
 @Component({
     name: 'SocialSharing',
 })
-export class SocialSharing extends Vue implements SocialSharingProps {
-    @Prop({ type: Array, required: true }) items!: SocialSharingProps['items'];
+
+export class SocialSharing extends Vue {
+    get items() {
+        return [
+            {
+                type: 'linkedin',
+                iconName: 'linkedin',
+                label: 'Share on Linkedin',
+            },
+            {
+                type: 'facebook',
+                iconName: 'facebook',
+                label: 'Share on Facebook',
+            },
+            {
+                type: 'twitter',
+                iconName: 'twitter',
+                label: 'Share on Twitter',
+            },
+        ];
+    }
+
+    url({ type }) {
+        const url = BrowserCapabilities.isBrowser
+            ? window.location.href
+            : '#';
+
+        return generateSocialSharingUrl(url, type);
+    }
 }
