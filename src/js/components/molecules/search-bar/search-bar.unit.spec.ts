651a1d1be844a152bf7d451c58f129a91d889079
dan entous <d.entous@ultimaker.com>
2019-07-29 12:01:38 +0200

    fix(): tests

--- src/js/components/molecules/search-bar/search-bar.unit.spec.ts
+++ src/js/components/molecules/search-bar/search-bar.unit.spec.ts
@@ -17,9 +17,9 @@ describe('components', () => {
             it('should render a search bar', () => {
                 const vm = mount();
 
-                const searchButton = vm.$el.querySelectorAll('.search__button');
+                const searchButton = vm.$el.querySelectorAll('.search-bar__button');
 
-                expect(vm.$refs.search__input.nodeName).toEqual('INPUT');
+                expect(vm.$refs['search-bar__input'].nodeName).toEqual('INPUT');
                 expect(vm.searchValue).toEqual('');
                 expect(searchButton.length).toEqual(1);
                 vm.$destroy();
@@ -28,32 +28,32 @@ describe('components', () => {
             it('should execute search on button click', () => {
                 const vm = mount();
 
-                spyOn(vm, 'doSearch');
+                spyOn(vm, 'handleSubmit');
                 vm.$mount();
 
                 // @ts-ignore
                 const clickEvent = new window.Event('click');
-                const searchButton = vm.$el.querySelector('.search__button');
+                const searchButton = vm.$el.querySelector('.search-bar__button');
 
                 vm.searchValue = 'something';
                 searchButton.dispatchEvent(clickEvent);
 
-                expect(vm.doSearch).toHaveBeenCalledTimes(1);
+                expect(vm.handleSubmit).toHaveBeenCalledTimes(1);
                 vm.$destroy();
             });
 
             it('should not execute search on button click if input is empty', () => {
                 const vm = mount();
 
-                spyOn(vm, 'doSearch');
+                spyOn(vm, 'handleSubmit');
                 vm.$mount();
 
                 // @ts-ignore
                 const clickEvent = new window.Event('click');
-                const searchButton = vm.$el.querySelector('.search__button');
+                const searchButton = vm.$el.querySelector('.search-bar__button');
 
                 searchButton.dispatchEvent(clickEvent);
-                expect(vm.doSearch).toHaveBeenCalledTimes(1);
+                expect(vm.handleSubmit).toHaveBeenCalledTimes(1);
                 // TODO: add test for search function when implemented
                 vm.$destroy();
             });
@@ -61,19 +61,19 @@ describe('components', () => {
             it('should focus on search input when focus has been called', () => {
                 const vm = mount();
 
-                spyOn(vm.$refs.search__input, 'focus');
+                spyOn(vm.$refs['search-bar__input'], 'focus');
                 vm.focusInput();
 
-                expect(vm.$refs.search__input.focus).toHaveBeenCalled();
+                expect(vm.$refs['search-bar__input'].focus).toHaveBeenCalled();
                 vm.$destroy();
             });
 
             it('should not focus on search input when focus has not been called', () => {
                 const vm = mount();
 
-                spyOn(vm.$refs.search__input, 'focus');
+                spyOn(vm.$refs['search-bar__input'], 'focus');
 
-                expect(vm.$refs.search__input.focus).not.toHaveBeenCalled();
+                expect(vm.$refs['search-bar__input'].focus).not.toHaveBeenCalled();
                 vm.$destroy();
             });
 
