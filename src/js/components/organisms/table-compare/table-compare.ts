95ebadcf3177ed73208092602f9064f84959a54e
Robin van Dijk <r.vandijk@developers.nl>
2019-08-26 10:04:11 +0200

    feat(table-compare): allow 5 columns
    Allow 5 columns to be compared on desktop.

--- src/js/components/organisms/table-compare/table-compare.ts
+++ src/js/components/organisms/table-compare/table-compare.ts
@@ -28,11 +28,19 @@ export default class TableCompare extends Vue implements TableCompareProps {
         scrollWidthContainer: HTMLElement;
         scrollContainer: HTMLElement;
         columns: HTMLElement[];
+        footers: HTMLElement[];
     };
+
+    visibilityClass: string = 'invisible';
+    initiallyVisible: number = 3;
 
     mounted() {
         this.resizeHandler = debounce(this.resetScrollPosition, 100);
         this.viewportUtility.addResizeHandler(this.resizeHandler);
+
+        if (!this.viewportUtility.isMobile) {
+            this.$refs.scrollContainer.addEventListener('scroll', this.scrollHandler);
+        }
 
         const options = {
             root: this.$refs.scrollWidthContainer,
@@ -45,10 +53,40 @@ export default class TableCompare extends Vue implements TableCompareProps {
         }
     }
 
+    get columnLength() {
+        return this.content.columns.length;
+    }
+
+    get visibilityOffset() {
+        const offSet = this.$refs.scrollContainer.offsetWidth;
+        return (offSet / this.columnLength) * (this.columnLength - this.initiallyVisible);
+    }
+
+    get showScrollButtons() {
+        return ((this.columnLength - this.initiallyVisible) > 0 && !this.viewportUtility.isMobile) ||
+            ((this.viewportUtility.isMobile || this.viewportUtility.isTablet) && this.columnLength > 1);
+    }
+
+    scrollHandler(): void {
+        if (!this.viewportUtility.isMobile) {
+            if (this.$refs.scrollContainer.scrollLeft <= this.visibilityOffset) {
+                this.toggleVisibility(this.$refs.footers, (this.columnLength - this.initiallyVisible), true);
+                this.toggleVisibility(this.$refs.columns, (this.columnLength - this.initiallyVisible), true);
+            } else {
+                this.toggleVisibility(this.$refs.footers, (this.columnLength - this.initiallyVisible), false);
+                this.toggleVisibility(this.$refs.columns, (this.columnLength - this.initiallyVisible), false);
+            }
+        }
+    }
+
     beforeDestroy() {
         this.resizeHandler.cancel();
         this.viewportUtility.removeResizeHandler(this.resizeHandler);
         this.observer.unobserve(this.$refs.scrollContainer);
+
+        if (!this.viewportUtility.isMobile) {
+            this.$refs.scrollContainer.removeEventListener('scroll', this.scrollHandler);
+        }
     }
 
     @Watch('content.columns')
@@ -85,11 +123,23 @@ export default class TableCompare extends Vue implements TableCompareProps {
         });
     }
 
-    scroll(reverse: boolean = false): void {
+    toggleVisibility(items: HTMLElement[], amount: number, show: boolean) {
+        items.forEach((item: HTMLElement, index :number) => {
+            if (index < amount && item) {
+                if (show) {
+                    item.classList.remove(this.visibilityClass);
+                } else {
+                    item.classList.add(this.visibilityClass);
+                }
+            }
+        });
+    }
+
+    async scroll(reverse: boolean = false): Promise<void> {
         const scrollWidth = this.$refs.scrollWidthContainer.clientWidth;
         const { scrollLeft } = this.$refs.scrollContainer;
 
-        this.$refs.scrollContainer.scrollTo({
+        await this.$refs.scrollContainer.scrollTo({
             left: scrollLeft + (reverse ? -1 : 1) * scrollWidth,
             behavior: 'smooth',
         });
