05a8a7b0d83d58b58655021355459dfdb13233f8
dan entous <d.entous@ultimaker.com>
2019-09-13 11:13:42 +0200

    feat(): add preferences-bar storybook template

---
+++ src/stories/templates/preferences-bar/data/preferences-bar.data.ts
@@ -0,0 +1,8 @@
+/** @format */
+import { data as searchResults } from 'components/organisms/search-results/data/search-results.data';
+
+export const data = {
+    default: {
+        searchResults: searchResults['with search results'],
+    },
+};
