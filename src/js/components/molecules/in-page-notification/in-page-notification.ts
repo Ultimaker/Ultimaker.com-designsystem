24395f471e081735b76ac53409c5d816e26909ae
Robin van Dijk <r.vandijk@developers.nl>
2019-09-19 14:13:31 +0200

    Code fixes

--- src/js/components/molecules/in-page-notification/in-page-notification.ts
+++ src/js/components/molecules/in-page-notification/in-page-notification.ts
@@ -1,16 +1,17 @@
 /** @format */
 import { Component, Prop, Vue } from 'vue-property-decorator';
 import WithRender from './in-page-notification.vue.html';
+import { Notification as NotificationInterface } from '@ultimaker/ultimaker.com-model-definitions/dist/molecules/notification/Notification';
 
 @WithRender
 @Component({
     name: 'InPageNotification',
 })
-export class InPageNotification extends Vue {
-    @Prop({ type: String, required: true }) message!: string;
+export class InPageNotification extends Vue implements NotificationInterface {
+    @Prop({ type: String, required: true }) message!: NotificationInterface['message'];
     public display: boolean = true;
 
-    public hide() {
+    public close(): void {
         this.display = false;
     }
 }
