377d4253baa8e968f0ad9de4db08883e6b2b7319
dan entous <d.entous@ultimaker.com>
2019-10-14 13:24:14 +0200

    fix(): search result
    * fix issue with regexp characters; e.g. ? [

---
+++ src/js/components/molecules/search-result/data/examples/ultimaker-s5.ts
@@ -0,0 +1,7 @@
+export const ultimakerS5 = {
+    terms: 'Ultimaker S5',
+    title: 'Update the S5 firmware',
+    link: 'https://ultimaker.com/en/resources/52601-update-the-firmware',
+    snippet: 'Periodically, a new version of the Ultimaker S5 firmware is released. To ensure that your Ultimaker S5 has the latest features, we recommend keeping the …',
+    formattedUrl: 'https://ultimaker.com/en/resources/52601-update-the-firmware',
+};
--- src/js/components/molecules/search-result/helpers/mark.ts
+++ src/js/components/molecules/search-result/helpers/mark.ts
@@ -1,8 +1,8 @@
-/** @format */
+import escapeStringRegexp from 'escape-string-regexp';
 
 export default function mark(text: string, terms: string): string {
     // find individual terms split on whitespace
-    const termsRE = new RegExp(`(${terms.split(/\s/).join('|')})`, 'gi');
+    const termsRE = new RegExp(`(${escapeStringRegexp(terms).split(/\s/).join('|')})`, 'gi');
 
     // wrap terms in <mark> tags
     return text.replace(termsRE, '<mark>$1</mark>');
