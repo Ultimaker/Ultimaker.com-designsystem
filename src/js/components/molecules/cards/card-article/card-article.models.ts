725acbaaba37475059546ac51417a6febea52cb6
Etiënne van den Tempel <e.vandentempel@ultimaker.com>
2019-10-25 15:25:10 +0200

    Convert card-article to single-file-component

--- src/js/components/molecules/cards/card-article/card-article.models.ts
+++
@@ -1,5 +0,0 @@
-/** @format */
-
-import { CardArticle as CardArticleInterface } from '@ultimaker/ultimaker.com-model-definitions/dist/molecules/cards/CardArticle';
-
-export interface CardArticleProps extends CardArticleInterface {}
--- src/js/components/molecules/cards/card-article/index.ts
+++
@@ -1,3 +0,0 @@
-/** @format */
-
-export { CardArticle as default } from './card-article';
