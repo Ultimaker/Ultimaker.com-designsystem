60b93853d73eba6fd56b6570e7e5f7d3abca9889
Etiënne van den Tempel <e.vandentempel@ultimaker.com>
2019-10-28 09:29:30 +0100

    Convert sys-nav to single-file-component

--- src/js/components/molecules/sys-nav/index.ts
+++
@@ -1,3 +0,0 @@
-/** @format */
-
-export { SysNav as default } from './sys-nav';
--- src/js/components/molecules/sys-nav/sys-nav.models.ts
+++
@@ -1,8 +0,0 @@
-/** @format */
-export interface SysNavProps {
-    orderNowIcon: string;
-    orderNowLink: string;
-    orderNowLabel: string;
-    searchIcon: string;
-    searchLabel: string;
-}
