9d43da8b2349100ca97a22a5d0eeb6455f53e473
Etiënne van den Tempel <e.vandentempel@ultimaker.com>
2019-11-21 14:33:00 +0100

    Simplify SysNav

--- src/js/components/molecules/sys-nav/SysNav.vue
+++ src/js/components/molecules/sys-nav/SysNav.vue
@@ -4,7 +4,7 @@
             <li class="sys-nav__item">
                 <base-link v-if="orderNowLink" :url="orderNowLink" class="sys-nav__link">
                     <icon class="sys-nav__icon" :icon-name="orderNowIcon"></icon>
-                    <span class="sys-nav__label sys-nav__label--hidden-sm">{{ orderNowLabel }}</span>
+                    <span class="sys-nav__label">{{ orderNowLabel }}</span>
                 </base-link>
             </li>
             <li class="sys-nav__item">
@@ -13,7 +13,7 @@
                    @keyup.enter="openSearch"
                    role="button" tabindex="0">
                     <icon class="sys-nav__icon" :icon-name="searchIcon"></icon>
-                    <span class="sys-nav__label sys-nav__label--hidden-sm">{{ searchLabel }}</span>
+                    <span class="sys-nav__label">{{ searchLabel }}</span>
                 </a>
             </li>
         </ul>
