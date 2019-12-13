20ad8b344872507c90a5872b699164ea09db3ca4
dan entous <d.entous@ultimaker.com>
2019-12-13 18:56:43 +0100

    remove(): ValueFilter

--- src/js/utils/filters/FilterInterface.ts
+++
@@ -1,7 +0,0 @@
-export interface FilterInterface {
-    // eslint-disable-next-line @typescript-eslint/no-explicit-any
-    items: any[];
-
-    // eslint-disable-next-line @typescript-eslint/no-explicit-any
-    filteredItems(): any[];
-}
