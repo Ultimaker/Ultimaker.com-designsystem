2b904adbecd31b7a0d804eb5f1ffa4a2294a156e
dan entous <d.entous@ultimaker.com>
2019-12-19 10:09:21 +0100

    remove(): flyout-section

--- src/js/components/molecules/index.js
+++ src/js/components/molecules/index.js
@@ -1,4 +1,3 @@
 import FilterCategory from 'molecules/filter-category/FilterCategory';
 import Flyout from 'molecules/flyout/Flyout';
-import FlyoutSection from 'molecules/flyout-section/FlyoutSection';
 import IconButton from 'molecules/icon-button/IconButton';
