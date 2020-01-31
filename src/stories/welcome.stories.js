d4316ff8614fac88d6c9151d5b952d0b7acf66fa
UltiSean <s.deleeuw@ultimaker.com>
2020-01-31 13:58:18 +0100

    remove(): storybook stories root

--- src/stories/welcome.stories.js
+++
@@ -1,9 +0,0 @@
-import { storiesOf } from '@storybook/vue';
-
-storiesOf('global', module)
-    .add(
-        'Welcome',
-        () => ({
-            template: '<div><h1>User Interface Patterns</h1><h2>Designed for Ultimaker.com</h2><p class="copy--intro">The patterns designed within this library are composed using the <a href="http://atomicdesign.bradfrost.com">Atomic Design</a> System principles created by Brad Frost.</p></div>',
-        }),
-    );
