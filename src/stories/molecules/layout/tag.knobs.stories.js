ac27b60f72d62a8a1f6b364bcede7fb0948ea687
UltiSean <s.deleeuw@ultimaker.com>
2020-01-31 14:02:43 +0100

    remove(): storybook stories molecules layout

--- src/stories/molecules/layout/tag.knobs.stories.js
+++
@@ -1,7 +0,0 @@
-/* eslint camelcase: ["error", {properties: "never"}] */
-import { text } from '@storybook/addon-knobs';
-import data from './tag.stories.json';
-
-export default () => ({
-    tag: text('Tag', data.title),
-});
